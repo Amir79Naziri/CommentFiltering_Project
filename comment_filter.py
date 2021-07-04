7a072b38871b585880e3f89ebc979119dedcaf23
aminhbl <amin.habibllh@gmail.com>
2021-07-04 19:10:28 +0430

    "feat: evaluation"

--- comment_filter.py
+++ comment_filter.py
@@ -8,7 +8,6 @@ class Model:
         self.neg_uniGram_dict = neg_dict[0]
         self.neg_biGram_dict = neg_dict[1]
 
-
     def estimate(self, line):
         pos_prob = 0.5 * probability(self.pos_uniGram_dict, self.pos_biGram_dict, line)
         neg_prob = 0.5 * probability(self.neg_uniGram_dict, self.neg_biGram_dict, line)
@@ -17,10 +16,9 @@ class Model:
 
 
 def probability(uni_dictionary, bi_dictionary, line):
-    l1 = 0.85
-    l2 = 0.10
-    l3 = 0.05
-    line = re.sub(r'[^A-Za-z0-9]+', ' ', line)
+    l1 = 0.95
+    l2 = 0.045
+    l3 = 0.005
     words = line.split()
     try:
         prob = uni_dictionary[line[0]] / len(uni_dictionary)
@@ -52,28 +50,25 @@ def read_dataSet(address):
 
 
 def preprocess():
-
     raw_pos_data = read_dataSet('DataSet/rt-polarity.pos')
     raw_neg_data = read_dataSet('DataSet/rt-polarity.neg')
 
     pos_lines = re.sub(r'[^A-Za-z0-9\n]+', ' ', raw_pos_data).split('\n')
     neg_lines = re.sub(r'[^A-Za-z0-9\n]+', ' ', raw_neg_data).split('\n')
 
+    pos_cutoff = int(0.98 * len(pos_lines))
+    neg_cutoff = int(0.98 * len(neg_lines))
 
-    pos_cutoff = int(0.95 * len(pos_lines))
-    neg_cutoff = int(0.95 * len(neg_lines))
+    pos_train_set = pos_lines[0:pos_cutoff]
+    pos_test_set = pos_lines[pos_cutoff:-1]
 
-    pos_train_set_ = pos_lines[0:pos_cutoff]
-    pos_test_set_ = pos_lines[pos_cutoff:-1]
+    neg_train_set = neg_lines[0:neg_cutoff]
+    neg_test_set = neg_lines[neg_cutoff:-1]
 
-    neg_train_set_ = neg_lines[0:neg_cutoff]
-    neg_test_set_ = neg_lines[neg_cutoff:-1]
-
-    return (pos_train_set_, pos_test_set_), (neg_train_set_, neg_test_set_)
+    return (pos_train_set, pos_test_set), (neg_train_set, neg_test_set)
 
 
 def train(pos_set, neg_set):
-
     def create_dict(lines):
         uniGram_dictionary = dict()
         biGram_dictionary = dict()
@@ -97,14 +92,27 @@ def train(pos_set, neg_set):
     return Model(create_dict(pos_set), create_dict(neg_set))
 
 
-def evaluate(model, test_set):
-    for line in test_set:
-        print(line)
+def evaluate(model, pos_test_set, neg_test_set):
+    TP = TN = FP = FN = 0
+    for line in pos_test_set:
         pos_prob, neg_prob = model.estimate(line)
         if pos_prob > neg_prob:
-            pass
+            TP += 1
         else:
-            pass
+            FN += 1
+
+    for line in neg_test_set:
+        pos_prob, neg_prob = model.estimate(line)
+        if pos_prob > neg_prob:
+            FP += 1
+        else:
+            TN += 1
+
+    recall = TP / (TP + FN)
+    precision = TP / (TP + FP)
+    accuracy = (TP + TN) / (TP + TN + FP + FN)
+    F1_score = (2 * precision * recall) / (precision + recall)
+    return recall, precision, accuracy, F1_score
 
 
 def run(model):
@@ -112,6 +120,7 @@ def run(model):
         line = input()
         if line == '!q':
             return
+        line = re.sub(r'[^A-Za-z0-9]+', ' ', line)
         pos_prob, neg_prob = model.estimate(line)
         if pos_prob > neg_prob:
             print("not filter this")
@@ -119,7 +128,13 @@ def run(model):
             print("filter this")
 
 
-if __name__ == '__main__':
+def main():
     (pos_train_set, pos_test_set), (neg_train_set, neg_test_set) = preprocess()
     my_model = train(pos_train_set, neg_train_set)
-    run(my_model)
+    # run(my_model)
+    recall, precision, accuracy, F1_score = evaluate(my_model, pos_test_set, neg_test_set)
+    print(recall, precision, accuracy, F1_score)
+
+
+if __name__ == '__main__':
+    main()
