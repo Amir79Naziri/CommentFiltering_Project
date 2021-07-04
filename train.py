ea3651197f5a720bb146b7e31d19fc3c2feb3a05
aminhbl <amin.habibllh@gmail.com>
2021-07-04 18:01:20 +0430

    "feat: train"

--- train.py
+++ train.py
@@ -1,6 +1,11 @@
 import re
 from random import shuffle
 
+POS_BiGRAM_DICT = dict()
+POS_UniGRAM_DICT = dict()
+NEG_BiGRAM_DICT = dict()
+NEG_UniGRAM_DICT = dict()
+
 
 def read_dataSet(address):
     try:
@@ -17,7 +22,6 @@ def create_dict(lines):
 
     for line in lines:
         line = re.sub(r'[^A-Za-z0-9]+', ' ', line)
-        line = '<s> ' + line + ' </s>'
         words = line.split()
         for i in range(len(words)):
             if words[i] not in uniGram_dictionary:
@@ -36,8 +40,10 @@ def create_dict(lines):
 
 def train():
 
-    pos_data = read_dataSet('DataSet/rt-polarity.neg')
-    neg_data = read_dataSet('DataSet/rt-polarity.pos')
+    global POS_BiGRAM_DICT, POS_UniGRAM_DICT, NEG_BiGRAM_DICT, NEG_UniGRAM_DICT
+
+    pos_data = read_dataSet('DataSet/rt-polarity.pos')
+    neg_data = read_dataSet('DataSet/rt-polarity.neg')
 
     pos_cutoff = int(0.95 * len(pos_data))
     neg_cutoff = int(0.95 * len(neg_data))
@@ -45,19 +51,70 @@ def train():
     pos_train_set = pos_data[0:pos_cutoff]
     neg_train_set = neg_data[0:neg_cutoff]
 
-    pos_uniGram_dict, pos_biGram_dict = create_dict(pos_train_set)
-    neg_uniGram_dict, neg_biGram_dict = create_dict(neg_train_set)
+    POS_UniGRAM_DICT, POS_BiGRAM_DICT = create_dict(pos_train_set)
+    NEG_UniGRAM_DICT, NEG_BiGRAM_DICT = create_dict(neg_train_set)
 
     test_set = pos_data[pos_cutoff:-1] + neg_data[neg_cutoff:-1]
     shuffle(test_set)
 
+    return test_set
+
+
+def test(test_set):
+
+    global POS_BiGRAM_DICT, POS_UniGRAM_DICT, NEG_BiGRAM_DICT, NEG_UniGRAM_DICT
+
+    for line in test_set:
+        print(line)
+        pos_prob = 0.5 * probability(POS_UniGRAM_DICT, POS_BiGRAM_DICT, line)
+        neg_prob = 0.5 * probability(NEG_UniGRAM_DICT, NEG_BiGRAM_DICT, line)
+        if pos_prob > neg_prob:
+            print("not filter this")
+        else:
+            print("filter this")
+
+
+def main():
+    global POS_BiGRAM_DICT, POS_UniGRAM_DICT, NEG_BiGRAM_DICT, NEG_UniGRAM_DICT
+
+    while True:
+        line = input()
+        if line == '!q':
+            return
+        pos_prob = 0.5 * probability(POS_UniGRAM_DICT, POS_BiGRAM_DICT, line)
+        neg_prob = 0.5 * probability(NEG_UniGRAM_DICT, NEG_BiGRAM_DICT, line)
+        if pos_prob > neg_prob:
+            print("not filter this")
+        else:
+            print("filter this")
+
+
+def probability(uni_dictionary, bi_dictionary, line):
+    l1 = 0.85
+    l2 = 0.10
+    l3 = 0.05
+    line = re.sub(r'[^A-Za-z0-9]+', ' ', line)
+    words = line.split()
+    try:
+        prob = uni_dictionary[line[0]] / len(uni_dictionary)
+    except KeyError:
+        prob = l3 * 0.5
+
+    for i in range(1, len(words)):
+        try:
+            biGram_prob = (bi_dictionary[(words[i - 1], words[i])] / uni_dictionary[words[i - 1]])
+        except KeyError:
+            biGram_prob = 0
+        try:
+            uniGram_prob = (uni_dictionary[words[i]] / len(uni_dictionary))
+        except KeyError:
+            uniGram_prob = 0
+        interpolation_prob = l1 * biGram_prob + l2 * uniGram_prob + l3 * 0.5
+        prob *= interpolation_prob
+
+    return prob
 
 
 if __name__ == '__main__':
-    # d = create_dict(read_dataSet('DataSet/rt-polarity.neg'))
-    # print(len(d))
-
-    t = {('a', 'b'): 0, ('c', 'd'): 0, ('a', 'b'): 0, ('e', 'f'): 0, ('a', 'b'): 0}
-
-    print(('x', 'f') in t)
-    print(t)
+    test_ = train()
+    main()
